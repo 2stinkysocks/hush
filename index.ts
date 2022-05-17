7ddc29a1b7b08b5be0b85fa0bba76694f543cf0d
2stinkysocks <zmehall@gmail.com>
2022-05-17 15:31:03 -0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+import { Client, Intents, User, Guild } from 'discord.js'
+import { REST } from '@discordjs/rest'
+import fs from 'fs'
+import dotenv from 'dotenv'
+import { Command } from './abstractcommand'
+
+dotenv.config()
+
+const client: Client = new Client({ intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS]})
+
+let commands: Array<Command> = new Array<Command>();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+	const c = require(`./commands/${file.replace('.ts', '')}`);
+	commands.push(new c())
+}
+
+client.once('ready', () => {
+    console.log("Bot has Started")
+    try {
+        if(process.env.ENV === "production") {
+            for(const command of commands) {
+                client.application?.commands.create(command.getJSON())
+                console.log("Registered global command: " + command.getName())
+            }
+        } else {
+            const guild = client.guilds.cache.get(process.env.TEST_GUILD_ID as string) as Guild
+            for(const command of commands) {
+                guild.commands.create(command.getJSON())
+                console.log("Registered local command: " + command.getName())
+            }
+        }
+    } catch(e) {
+        console.error(e)
+    }
+})
+
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return
+    for(const command of commands) {
+        if(command.getName() === interaction.commandName) {
+            command.execute(interaction)
+        }
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
